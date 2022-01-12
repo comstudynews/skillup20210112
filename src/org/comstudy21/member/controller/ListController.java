d470b58d1b8ccb033a8182ac5630b4277f9d4e02
comstudynews <comsutdynews@gmail.com>
2022-01-12 20:32:05 +0900

    day21 skillup step02

--- src/org/comstudy21/member/controller/ListController.java
+++ src/org/comstudy21/member/controller/ListController.java
@@ -1,12 +1,17 @@
 package org.comstudy21.member.controller;
 
+import java.util.List;
+
+import org.comstudy21.member.model.MemberDTO;
 import org.comstudy21.member.view.MemberView;
 
 public class ListController extends Controller {
 
 	@Override
 	public MemberView hadleRequest() {
-		// TODO Auto-generated method stub
+		List<MemberDTO> memList = memDao.selectAll();
+		request.put("memList", memList);
+		
 		return list;
 	}
 
--- src/org/comstudy21/member/view/List.java
+++ src/org/comstudy21/member/view/List.java
@@ -1,10 +1,18 @@
 package org.comstudy21.member.view;
 
+
+import org.comstudy21.member.model.MemberDTO;
+
 public class List extends MemberView {
 	@Override
 	public boolean onCreate() {
 		System.out.println(titleList);
 		
+		// request에서 List를 가져오기
+		java.util.List<MemberDTO> memList = (java.util.List<MemberDTO>)request.get("memList");
+		for(MemberDTO member : memList) {
+			System.out.println(member);
+		}
 		
 		return false;
 	}
